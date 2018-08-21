fe85735c6de9f1503f4fe6b9d3c2ad8c1b66b8a1
austin <austin.grubb.1995@gmail.com>
2018-08-21 10:45:29 -0700

    working on the routes

--- __instructor-code__/src/components/App.js
+++ __instructor-code__/src/components/App.js
@@ -2,19 +2,20 @@ import React, { Component } from 'react';
 import Home from './Home/Home';
 import About from './About/About';
 
-
 class App extends Component {
-  componentDidMount(){
-    window.addEventListener("hashchange", () => this.forceUpdate())
+  componentDidMount() {
+    window.addEventListener('hashchange', () => this.forceUpdate());
   }
+  
   render() {
     return (
       <div>
+        This is App.js
         <div>
-          <a href= '/#'>Home</a>
-          <a href= '/#/about'>About</a>
+          <a href="/#">Home</a>
+          <a href="/#/about">About</a>
         </div>
-        {window.location.hash === '#/about' ? <About/> : <Home/>}
+        {window.location.hash === '#/about' ? <About /> : <Home />}
       </div>
     );
   }
